9f799d25cd7ab768f81f31d8b0c4705413816736
Surya <Surya.movva999@gmail.com>
2016-06-16 10:18:30 +0530

    sysouts removed

--- HiWorld/src/HiWorld.java
+++ HiWorld/src/HiWorld.java
@@ -4,10 +4,7 @@ public class HiWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("hello world");
-			System.out.println("change committed");
-			System.out.println("last");
-			System.out.println(".........");
-			System.out.println("new change appended");
+			
 	}
 
 }
